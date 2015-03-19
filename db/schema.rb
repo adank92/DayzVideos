5672cbc7deb51b6d220aef2385d0202ba718e818
Adan Koch <adank92@gmail.com>
2015-03-19 17:49:07 -0300

    Added Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318171526) do
+ActiveRecord::Schema.define(version: 20150319204658) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string   "title"
