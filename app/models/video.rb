4582bb10d34f562d8ea550098f1ef49679fffa80
adan <adank92@gmail.com>
2015-03-23 19:42:10 -0300

    youtube call for video info

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,17 @@
 class Video < ActiveRecord::Base
   has_many :video_categories
   has_many :categories, through: :video_categories
+  validates :name, presence: true
+
+  before_save :fetch_youtube_info
+
+  protected
+
+  def fetch_youtube_info
+    yt_client = YouTubeIt::Client.new(dev_key: Rails.application.secrets.youtube_key)
+    yt_video = yt_client.video_by(youtube_id)
+    self.title = yt_video.title
+    self.uploader = yt_video.author.name
+    self.duration = yt_video.duration
+  end
 end
