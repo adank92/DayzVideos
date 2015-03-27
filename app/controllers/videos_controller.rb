dfb2f32619505b7bcdd787f7cb90bcd9bc3a7c16
adan <adank92@gmail.com>
2015-03-26 21:08:52 -0300

    Form for video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -69,6 +69,6 @@ class VideosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def video_params
-      params.require(:video).permit(:title, :url, :uploader, :duration)
+      params.require(:video).permit(:youtube_id)
     end
 end
