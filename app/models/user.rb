e8b3cdc42e35412a6948f13e4599ea400a591794
adan <adank92@gmail.com>
2015-04-11 19:07:04 -0300

    Ordering validations in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
   before_save :downcase_email
   has_secure_password
-  validates :password, length: { minimum: 6 }
-
+  
   def downcase_email
     self.email = email.downcase
   end
