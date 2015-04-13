0d0abc2f8cadcb5c2814da83a8269713df1aba64
adan <adank92@gmail.com>
2015-04-13 19:22:53 -0300

    User signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def new
@@ -9,7 +9,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
 
+private
+
+  def user_params
+    binding.pry
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
