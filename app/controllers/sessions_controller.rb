e9b0189a308787067093413770b2443f5209b478
Adan Koch <adank92@gmail.com>
2015-04-15 17:58:31 -0300

    Login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash[:danger] = "Invalid info"
+      render :new
+    end
+  end
+
+  def destroy
+    
+  end
 end
