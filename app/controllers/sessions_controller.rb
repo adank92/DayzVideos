1f7e417322e0e5fa0596620ed51304046afe57bf
Adan Koch <adank92@gmail.com>
2015-04-17 16:39:49 -0300

    Session management

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,26 @@
 class SessionsController < ApplicationController
   def new
+    respond_to do |format|
+      format.html # default
+      format.js # modal
+    end
   end
 
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-
+      flash[:success] = "Welcome back Survivor, have fun!"
+      log_in user
+      redirect_to user
     else
-      flash.now[:danger] = "Invalid info"
+      flash.now[:danger] = "Invalid login info, have the zombies eaten your brain?"
       render :new
     end
   end
 
   def destroy
-    
+    log_out
+    flash[:success] = 'You are now logged out, see you in Chernarus!'
+    redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session[:user_id] = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
