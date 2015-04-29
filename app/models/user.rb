8dd53eb8ac4e8af735c53932937e98372053952e
Adan Koch <adank92@gmail.com>
2015-04-29 11:35:41 -0300

    Account activation refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,15 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
