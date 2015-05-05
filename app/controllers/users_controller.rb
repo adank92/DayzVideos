362f28b8925e748178436be9ea8ff48f7e7a3f18
adan <adank92@gmail.com>
2015-05-04 22:45:43 -0300

    Move login filter to the top level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,15 +55,6 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = 'Please log in'
-        redirect_to login_path
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
