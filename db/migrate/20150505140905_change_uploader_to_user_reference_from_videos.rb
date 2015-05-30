415a0cfc6ef5291ad764a0d82e7c837599babb0a
adan <adank92@gmail.com>
2015-05-29 22:46:51 -0300

    Migration modification for Posgre

--- db/migrate/20150505140905_change_uploader_to_user_reference_from_videos.rb
+++ db/migrate/20150505140905_change_uploader_to_user_reference_from_videos.rb
@@ -1,11 +1,11 @@
 class ChangeUploaderToUserReferenceFromVideos < ActiveRecord::Migration
   def up
     rename_column :videos, :uploader, :user_id
-    change_column :videos, :user_id, :integer
+    change_column :videos, :user_id, 'integer USING CAST(user_id AS integer)'
   end
 
   def down
-    change_column :videos, :user_id, :string
+    change_column :videos, :user_id, 'string USING CAST(user_id AS string)'
     rename_column :videos, :user_id, :uploader
   end
 end
