e4dd9638c5371bad94b4931ea5b8c7151c570eab
Adan Koch <adank92@gmail.com>
2015-06-03 10:59:51 -0300

    Zoho implementation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,18 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.zoho.com",
+    :port                 => 465,
+    :user_name            => ENV['ZOHO_EMAIL'],
+    :password             => ENV['ZOHO_PASS'],
+    :authentication       => :login,
+    :ssl                  => true,
+    :tls                  => true,
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { host: 'dayzvideos.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
